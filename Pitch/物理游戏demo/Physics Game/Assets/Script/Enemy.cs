76826f003f023fd0bc34e7f74da960fc55254d0a
Aieylisyh <Aieylisyh@users.noreply.github.com>
2021-10-19 15:40:54 +0800

    物理游戏wip

--- Pitch/物理游戏demo/Physics Game/Assets/Script/Enemy.cs
+++ Pitch/物理游戏demo/Physics Game/Assets/Script/Enemy.cs
@@ -6,37 +6,52 @@ public class Enemy : MonoBehaviour
 {
     private GameObject player;
     private Rigidbody rb;
+    private float _timer;
 
-    public float distance;
-    public float force;
-    public float timer;
+    [Tooltip("AI config")]
+    public float turnTime=2;
+    public float force=10000;
+
+    [Range(0, 45)]
+    public float angleVariantPercent;
+    [Range(0, 50)]
+    public float forceVariantPercent;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
-        timer = 2;
+        _timer = turnTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (GameManager.instance.gameMode == GameManager.GameMode.Enemy)
-            timer -= Time.deltaTime;
+            _timer -= Time.deltaTime;
 
-        if (timer < 0)
+        if (_timer < 0)
             Attack();
     }
 
     private void Attack()
     {
         Vector3 direction = player.transform.position - transform.position;
-        distance = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized;
+        //direction是敌人指向我的单位向量，去除了y轴干扰
+        var newAngle = Quaternion.AngleAxis(Random.Range(-angleVariantPercent, angleVariantPercent), Vector3.up);
+        var baseAngle = Quaternion.LookRotation(direction, Vector3.up);
+        var resultAngle = newAngle * baseAngle;
+        //两个Quaternion相乘，就是把他们对应的角度依次变换，得到的是最终的角度Quaternion
 
-        rb.AddForce(direction * force * 10000);
-        Debug.Log("Attack");
+        var realForce = force * (Random.Range(-forceVariantPercent, forceVariantPercent) / 100f + 1);
+        //百分比调整后的力度
+        var finalDirection = resultAngle * Vector3.forward;
+        //resultAngle是一个Quaternion，乘以Vector3.forward，得到这个Quaternion的forward对应的箭头的向量
+
+        rb.AddForce(finalDirection * force);
 
         GameManager.instance.gameMode = GameManager.GameMode.Player;
-        timer = 2;
+        _timer = turnTime;
     }
 }
