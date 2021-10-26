4bd5deb2944f52cd9562fbcdf5171135a43c18d2
Aieylisyh <Aieylisyh@users.noreply.github.com>
2021-10-26 11:27:52 +0800

    状态机xmind playermove改时间流速

--- Pitch/2D战斗类demo/2DFight/Assets/Scripts/PlayerMove.cs
+++ Pitch/2D战斗类demo/2DFight/Assets/Scripts/PlayerMove.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MyTime
+{
+    public static float timescale = 1;
+    public static float deltaTime
+    {
+        get
+        {
+            return Time.deltaTime * timescale;
+        }
+    }
+}
+
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    public Transform slowMotionTarget;
+
     private float velocityX;
 
     public float speed;
@@ -25,17 +39,27 @@ public class PlayerMove : MonoBehaviour
     //2D移动
     void movement()
     {
+        var dt = MyTime.deltaTime;
+        if (Vector3.Distance(transform.position, slowMotionTarget.position) > 3)
+        {
+            dt = dt;
+        }
+        else
+        {
+            dt = dt * 0.25f;
+        }
+
         //移动
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             rb.velocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x,
-                speed * Time.fixedDeltaTime * 60, ref velocityX, 0.1f), rb.velocity.y);
+                speed * dt * 60, ref velocityX, 0.1f), rb.velocity.y);
             //anim.SetBool("isMoving", true);
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
             rb.velocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x,
-                speed * Time.fixedDeltaTime * -60, ref velocityX, 0.1f), rb.velocity.y);
+                speed * dt * -60, ref velocityX, 0.1f), rb.velocity.y);
             //anim.SetBool("isMoving", true);
         }
         else
@@ -48,7 +72,7 @@ public class PlayerMove : MonoBehaviour
 
         //转向
         float horizontalMove = Input.GetAxisRaw("Horizontal");
-        if(horizontalMove!=0)
+        if (horizontalMove != 0)
             transform.localScale = new Vector3(horizontalMove, 1, 1);
     }
 }
