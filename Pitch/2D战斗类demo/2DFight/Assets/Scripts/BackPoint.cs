ac39a6dea1b2f33be8f1be4f1210c66794f611e8
Oliver_Di <980720487@qq.com>
2021-11-15 21:30:43 +0800

    2D新陷阱
    摆锤陷阱
    遗留问题：基于物理引擎的摆动幅度会逐渐减小

---
+++ Pitch/2D战斗类demo/2DFight/Assets/Scripts/BackPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPoint : MonoBehaviour
+{
+    public Transform backPos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = backPos.position;
+        }
+    }
+}
---
+++ Pitch/2D战斗类demo/2DFight/Assets/Scripts/BladeTrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeTrap : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //额外施加力
+            Vector2 dir = collision.transform.position - transform.position;
+            collision.transform.GetComponent<PlayerGetHit>().GetHitBack(1, dir, 100);
+            //眩晕主角
+            collision.transform.GetComponent<PlayerGetHit>().Vertigo();
+        }
+    }
+}
