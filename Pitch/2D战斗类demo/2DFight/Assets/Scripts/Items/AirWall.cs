89235207d2f71e498bcdf9e52585dca7358a0ad6
Oliver_Di <980720487@qq.com>
2021-12-12 16:10:03 +0800

    2D bug修复以及细节更改
    彩蛋修改
    一些bug修复

--- Pitch/2D战斗类demo/2DFight/Assets/Scripts/Items/AirWall.cs
+++ Pitch/2D战斗类demo/2DFight/Assets/Scripts/Items/AirWall.cs
@@ -16,9 +16,6 @@ public class AirWall : MonoBehaviour
             Popup();
             popupDone = true;
         }
-
-        if(walker.activeSelf==false)
-            GetComponent<BoxCollider2D>().enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
