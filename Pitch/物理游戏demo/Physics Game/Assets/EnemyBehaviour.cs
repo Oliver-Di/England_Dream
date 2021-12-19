4e4719f42a3a1da397b26752c252f72dc772bd2c
Oliver_Di <980720487@qq.com>
2021-12-19 23:56:01 +0800

    物理 AI和UI
    AI基本完成，数值待配置
    UI完成框架

--- Pitch/物理游戏demo/Physics Game/Assets/EnemyBehaviour.cs
+++ Pitch/物理游戏demo/Physics Game/Assets/EnemyBehaviour.cs
@@ -11,6 +11,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     private float distance;
     private GameObject selectedObj;
+    private List<GameObject> canAttackObj;
 
     void Start()
     {
@@ -38,7 +39,7 @@ public class EnemyBehaviour : MonoBehaviour
         if (WinAreaManager.instance.players.Count != 0)
         {
             //选定Area里的第一个Player为目标，找到其最近的enemy
-            FindNearestObj(WinAreaManager.instance.players[0].transform);
+            FindNearestObj(WinAreaManager.instance.players[0].transform,enemys);
             //进行撞击
             selectedObj.GetComponent<Enemy>().target = WinAreaManager.instance.players[0];
             selectedObj.GetComponent<Enemy>().canAttack = true;
@@ -48,17 +49,35 @@ public class EnemyBehaviour : MonoBehaviour
             //不存在player但存在enemy
             if (WinAreaManager.instance.enemys.Count != 0)
             {
-                //随机一个目标
-                int rand = Random.Range(0, players.Count);
-                FindNearestObj(players[rand].transform);
-                //撞击
-                selectedObj.GetComponent<Enemy>().target = players[rand];
-                selectedObj.GetComponent<Enemy>().canAttack = true;
+                //建立一个不在Area的组
+                canAttackObj = enemys;
+                for (int i = 0; i < WinAreaManager.instance.enemys.Count; i++)
+                {
+                    canAttackObj.Remove(WinAreaManager.instance.enemys[i]);
+                }
+                if (canAttackObj.Count == 0)
+                {
+                    //如果组为空，则随机一个目标
+                    int rand = Random.Range(0, players.Count);
+                    FindNearestObj(players[rand].transform, enemys);
+                    //撞击
+                    selectedObj.GetComponent<Enemy>().target = players[rand];
+                    selectedObj.GetComponent<Enemy>().canAttack = true;
+                }
+                else
+                {
+                    //如果组不为空，则随机一个目标用组内obj攻击
+                    int rand = Random.Range(0, players.Count);
+                    FindNearestObj(players[rand].transform, canAttackObj);
+                    //撞击
+                    selectedObj.GetComponent<Enemy>().target = players[rand];
+                    selectedObj.GetComponent<Enemy>().canAttack = true;
+                }
             }
             else    //不存在任何obj
             {
                 //找到离Area最近的enemy，占领区域
-                FindNearestObj(areaPos);
+                FindNearestObj(areaPos,enemys);
                 //进行撞击
                 selectedObj.GetComponent<Enemy>().target = areaPos.gameObject;
                 selectedObj.GetComponent<Enemy>().canAttack = true;
@@ -66,22 +85,22 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
-    private void FindNearestObj(Transform target)
+    private void FindNearestObj(Transform target,List<GameObject> list)
     {
-        for (int i = 0; i < enemys.Count; i++)
+        for (int i = 0; i < list.Count; i++)
         {
-            float dis = Vector3.Distance(enemys[i].transform.position, target.position);
+            float dis = Vector3.Distance(list[i].transform.position, target.position);
             if (i == 0)
             {
                 distance = dis;
-                selectedObj = enemys[i];
+                selectedObj = list[i];
             }
             else
             {
                 if (dis < distance)
                 {
                     distance = dis;
-                    selectedObj = enemys[i];
+                    selectedObj = list[i];
                 }
             }
         }
